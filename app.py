33620a129f3c13cafc7ca8acbb3caaf2bc1bf763
Alvin Shrestha <alvin@Alvins-MacBook-Pro.local>
2022-10-27 22:40:09 +0800

    created v2

--- app.py
+++ app.py
@@ -23,3 +23,27 @@ def index():
         res = None
         
     return render_template('index.html', answer=res)
+
+@app.route('/v2/')
+def v2():
+    get_arg = lambda x: request.args.get(x, '')
+    
+    day = get_arg('day')
+    month = get_arg('month')
+    year = get_arg('year')
+    date_str = f"{day}/{month}/{year}"
+    
+    try:
+            date = datetime.strptime(date_str, "%d/%m/%Y")
+            date += timedelta(days=1)
+            res = date.strftime("%d %B %Y")
+    except Exception as e:
+        res = "Something went wrong... Check your input"
+    
+    if date_str == "//":
+        res = None
+    
+    if day == '28' and month == '2':
+        res = f'29 February {year}'
+        
+    return render_template('index2.html', answer=res)
