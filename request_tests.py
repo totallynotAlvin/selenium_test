9f17c44f1c4357e5d8d54e8582e358dc98cf47d5
Sewen Thy <sewen.thy@ahrefs.com>
2022-10-27 23:46:43 +0800

    finished v1 and v2 tests

--- request_tests.py
+++ request_tests.py
@@ -2,15 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def make_request(url):
+def make_request(url, expected):
     res = requests.get(url)
     print(f'for request to: {url}; status: {res.status_code}')
-    soup = BeautifulSoup(res.content)
-    print(soup.find('h2', {'id': 'answer'}).prettify())
+    soup = BeautifulSoup(res.content, "html.parser")
+    answer = soup.find('h2', {'id': 'answer'})
+    print(answer.prettify())
+    if expected in answer.text:
+        print("passed")
+    else:
+        print("failed")
     print()
-    print()
 
 
-make_request('https://next-day.herokuapp.com/?day=1&month=2&year=2000')
+print("v1 tests")
+make_request('https://next-day.herokuapp.com/?day=1&month=2&year=2000', "02 February 2000")
 
-make_request('https://next-day.herokuapp.com/?day=a&month=b&year=c')
+make_request('https://next-day.herokuapp.com/?day=a&month=b&year=c', "Something went wrong... Check your input")
+
+make_request('https://next-day.herokuapp.com/?day=28&month=2&year=2022', "01 March 2022")
+
+print("v2 tests")
+
+make_request('https://next-day.herokuapp.com/v2/?day=1&month=2&year=2000', "02 February 2000")
+
+make_request('https://next-day.herokuapp.com/v2/?day=a&month=b&year=c', "Something went wrong... Check your input")
+
+make_request('https://next-day.herokuapp.com/v2/?day=28&month=2&year=2022', "01 March 2022")
--- selenium_tests.py
+++ selenium_tests.py
@@ -1,15 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
 from time import sleep
 
-driver = webdriver.Chrome()
-url = "https://next-day.herokuapp.com"
-driver.get(url)
 
-def send_date(day_txt, month_txt, year_txt, clear=True):
+def send_date(driver, day_txt, month_txt, year_txt, clear=True):
     
-
     day = driver.find_element(By.ID, "day")
     month = driver.find_element(By.ID, "month")
     year = driver.find_element(By.ID, "year")
@@ -23,19 +20,56 @@ def send_date(day_txt, month_txt, year_txt, clear=True):
     month.send_keys(month_txt)
     year.send_keys(year_txt)
 
+    sleep(2)
+
     submit = driver.find_element(By.ID, "submit")
     submit.click()
+    sleep(1)
+    Alert(driver).accept()
 
 
-send_date("a", "b", "c")
-assert "Something went wrong... Check your input" not in driver.page_source
-sleep(1)
+def test_suite(driver, url):
+    driver.get(url)
 
-send_date("1", "2", "1992")
-assert "02 February 1992" in driver.page_source
-sleep(1)
+    send_date("a", "b", "c")
+    sleep(2)
+    assert "Something went wrong... Check your input" not in driver.page_source
+    input("please press enter to continue")
 
-send_date("2", "2", "1992")
-assert "03 February 1992" in driver.page_source
-sleep(1)
-# driver.close()
+    send_date("1", "2", "1992")
+    assert "02 February 1992" in driver.page_source
+    sleep(2)
+
+    send_date("2", "2", "1992")
+    assert "03 February 1992" in driver.page_source
+    input("please press enter to continue")
+
+    day = driver.find_element(By.ID, "day")
+    day.send_keys(Keys.DOWN)
+    day.send_keys(Keys.DOWN)
+    day.send_keys(Keys.DOWN)
+    sleep(3)
+
+    submit = driver.find_element(By.ID, "submit")
+    submit.click()
+    input("please press enter to continue")
+
+    assert "Something went wrong... Check your input" in driver.page_source
+    sleep(2)
+
+    send_date("28", "2", "2022")
+    assert "01 March 2022" in driver.page_source
+
+
+driver = webdriver.Chrome()
+url_v1 = "https://next-day.herokuapp.com"
+input("please press enter to start v1 tests")
+test_suite(driver, url_v1)
+
+input("please press enter to continue to v2 tests")
+
+url_v2 = "https://next-day.herokuapp.com/v2"
+test_suite(driver, url_v2)
+
+input("please press enter to close")
+driver.close()
